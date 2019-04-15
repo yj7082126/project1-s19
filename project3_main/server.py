56fa7072343952478f009c93e96584a9e1c4665a
yj7082126 <yk7da@virginia.edu>
2019-04-15 10:58:59 -0400

    temporary

--- project3_main/server.py
+++ project3_main/server.py
@@ -584,7 +584,8 @@ if __name__ == "__main__":
 
     HOST, PORT = host, port
     print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    #app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+	app.run()
 
 
   run()
